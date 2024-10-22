c005710174dd575bd1a3442024a7750831bbf9da
wlcsv <wlcsv8080@outlook.com>
2024-10-21 21:46:10 -0300

    Rin

--- Rinnegan.ino
+++ Rinnegan.ino
@@ -9,19 +9,35 @@
 #define TFT_SCLK 18 
 #define TFT_CS  5  
 
+// Color definitions
+#define BLACK        0x0000
+#define DARK_PURPLE  0x381F
+
 Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
 
 void setup(void) {
   Serial.begin(9600);
 
   tft.init(240, 240, SPI_MODE3); // initialize a ST7789 chip, 240×240 pixels
-  tft.fillScreen(ST77XX_BLACK);
-  // tft.setCursor(20, 50);
-  tft.setTextColor(ST77XX_YELLOW);
-  tft.setTextSize(2);
-  tft.println("hello");
+  tft.setRotation(1);
+  tft.fillScreen(DARK_PURPLE); 
+
+  int radius = 10;
+
+  // Pupil
+  tft.drawCircle(120, 120, radius, BLACK);
+  tft.fillCircle(120, 120, radius, BLACK);
+
+  // 4 lines
+  for (int i = 0; i < 4; i++) {
+    radius += 30;
+    // Each line will be 5 pixels wide
+    for (int j = 0; j < 5; j++) {
+      tft.drawCircle(120, 120, radius + j, BLACK);
+    }
+  }
+
 }
 
 void loop (){
-
 }
